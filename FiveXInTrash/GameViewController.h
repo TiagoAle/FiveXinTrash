c8c00b2e36f07f2afa8dcf5e2bfc729b08c3b047
Samuel <samuel.honorato.silva@gmail.com>
2016-05-10 09:21:54 -0300

    Initial Commit

---
+++ FiveXInTrash/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  FiveXInTrash
+//
+
+//  Copyright (c) 2016 Tiago Alexandre Francisco de Queiroz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ FiveXInTrash/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FiveXInTrash
+//
+//  Created by Tiago Alexandre Francisco de Queiroz on 10/05/16.
+//  Copyright © 2016 Tiago Alexandre Francisco de Queiroz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
